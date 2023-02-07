71abc443b669fcdcf0e06e2c0f6687d600b93130
s-matsubara <mtytsy15301217@gmail.com>
2023-02-07 16:33:23 +0900

    a

--- js/oneday.js
+++ js/oneday.js
@@ -1,18 +1,18 @@
-const welfare_swiper = new Swiper('.swiper', {
+const welfare_swiper = new Swiper('.welfare__swiper', {
   loop: true,
   effect: 'fade',
   fadeEffect: {
     crossFade: true,
   },
   speed: 2000,
-  autoplay: {
-    delay: 8000,
-    disableOnInteraction: false,
-    waitForTransition: false,
-  },
+  // autoplay: {
+  //   delay: 8000,
+  //   disableOnInteraction: false,
+  //   waitForTransition: false,
+  // },
 
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+  pagination: {
+    el: '.welfare__swiper-pagination',
+    clickable: true,
   },
 });
